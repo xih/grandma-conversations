8e851c4eb351e3a5bc7141ad51e0aaf157ee61b3
Dennis <dennis@momentum.local>
2024-05-22 21:20:25 -0700

    add Recoleta and Open Sans as fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,20 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import localFont from "next/font/local";
 
 const inter = Inter({ subsets: ["latin"] });
 
+// Font files can be colocated inside of `app`
+const myFont = localFont({
+  src: "../../public/Recoleta-Alt-Bold.otf",
+  display: "swap",
+});
+
 export const metadata: Metadata = {
   title: "Grandma Pentest",
-  description: "AI scams are everywhere. Protect your loved ones with Grandma Pentest.",
+  description:
+    "AI scams are everywhere. Protect your loved ones with Grandma Pentest.",
 };
 
 export default function RootLayout({
